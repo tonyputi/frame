a01697b4fd9b1aa9d90c01f3c8e7e5c8ea9f4bf0
Javier De la Hoz <javier.delahos@videoslots.com>
2021-02-17 15:29:51 +0100

    welcome page

--- resources/js/Pages/GameProviders.vue
+++ resources/js/Pages/GameProviders.vue
@@ -2,7 +2,7 @@
     <app-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Casino Providers
+                Game Providers
             </h2>
         </template>
 
