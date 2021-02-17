bb722bb2547a286a576f3c1723fb9f69cfd92581
Antonio <antonio.blazquez@videoslots.com>
2021-02-17 16:49:43 +0100

    add Vue table

--- resources/js/Pages/Reservations.vue
+++ resources/js/Pages/Reservations.vue
@@ -10,7 +10,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
                 <search-form></search-form>
-
+                <vue-table :header="header" :content="content"></vue-table>
 
             </div>
         </div>
@@ -21,16 +21,32 @@
 import AppLayout from '@/Layouts/AppLayout';
 import Welcome from '@/Jetstream/Welcome';
 import SearchForm from "@/Pages/GameProviders/SearchForm";
+import VueTable from '@/Shared/VueTable';
 
 export default {
     components: {
         AppLayout,
         Welcome,
-        SearchForm
+        SearchForm,
+        VueTable
+    },
+
+    data() {
+        return {
+            header: [
+                'id', 'environment_id', 'application_id', 'Game Provider', 'Host', 'Notes',  'Started at', 'Ended at', 'applied_at' , 'Is Active', 'created_at', 'updated_at'
+            ],
+            queue: null,
+            content: null
+        }
+    },
+
+    mounted() {
+        this.content = this.gameProviderQueue.data
     },
 
     props: {
-      casinoProviders: Array
+      gameProviderQueue: Array
     }
 };
 </script>
